890f7cb373abdaf52643424cd5cf2a8610188d71
lolilijve <yj13456213632@gmail.com>
2018-08-27 10:04:30 +0800

    --yj

--- src/main/java/com/spider/common/xss/XssFilter.java
+++ src/main/java/com/spider/common/xss/XssFilter.java
@@ -1,20 +1,26 @@
 package com.spider.common.xss;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * XSS过滤
- * 
+ *
  * @author maoxinmin
  */
 public class XssFilter implements Filter {
 
 	@Override
-	public void init(FilterConfig config) throws ServletException {
+	public void init(FilterConfig config) {
 	}
 
+	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		XssHttpServletRequestWrapper xssRequest = new XssHttpServletRequestWrapper((HttpServletRequest) request);
